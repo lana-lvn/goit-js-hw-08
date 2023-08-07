9997d9bbd2728d177e6804ea08e378d8d6ac8ab7
lana-lvn <ruslana.lohvyn@gmail.com>
2023-08-07 22:03:17 +0300

    3rd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+function saveCurrentTimeKey(timeKey) { 
+    localStorage.setItem(CURRENT_TIME_KEY, timeKey);
+};
+
+player.on('timeupdate', throttle(() => {
+    player.getCurrentTime().then((time) => {
+        saveCurrentTimeKey(time);
+    });
+}, 1000));
+
+const currentTimeKey = Number(localStorage.getItem(CURRENT_TIME_KEY)) || 0;
+
+player.setCurrentTime(currentTimeKey);
