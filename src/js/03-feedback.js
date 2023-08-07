31741156e57b25c0237f0833150b73c98c9b2eae
lana-lvn <ruslana.lohvyn@gmail.com>
2023-08-07 20:47:51 +0300

    2nd task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('textarea'),
+};
+
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextInput, 500))
+
+fillTextarea();
+
+function onTextInput(evt) { 
+
+    
+    formData[evt.target.name] = evt.target.value;
+    console.log(formData);
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(evt) { 
+    evt.preventDefault();
+    
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+
+function fillTextarea() { 
+    const savedData = localStorage.getItem(STORAGE_KEY);
+
+    if (savedData) { 
+        
+    const parsedData = JSON.parse(savedData);
+
+    refs.textarea.value = parsedData.message;
+    refs.form.email.value = parsedData.email;
+    };
+};
